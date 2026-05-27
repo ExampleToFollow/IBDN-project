f18809b3d78328cbbe58f736bf0cd8b3bd0c51f7
ExampleToFollow <a20213704@pucp.edu.pe>
2026-05-27 10:09:12 +0200

    Almost done

---
+++ k8s/ola-2.sh
@@ -0,0 +1,5 @@
+docker push hineill/ibdn-flask-web:latest
+docker push hineill/ibdn-spark-train:latest
+docker push hineill/ibdn-spark-predictor:latest
+docker push hineill/ibdn-cassandra-init:latest
+docker push hineill/ibdn-minio-init:latest
